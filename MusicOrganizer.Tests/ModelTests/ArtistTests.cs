738194a606a6e32a438816a48931cee99f964b9c
Aidan Williams <aidanwilliams01@gmail.com>
2023-10-24 15:49:12 -0700

    pass artist tests

---
+++ MusicOrganizer.Tests/ModelTests/ArtistTests.cs
@@ -0,0 +1,45 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MusicOrganizer.Models;
+using System.Collections.Generic;
+
+namespace MusicOrganizer.Tests
+{
+  [TestClass]
+  public class ArtistTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Artist.ClearAll();
+    }
+
+    [TestMethod]
+    public void ArtistConstructor_CreatesInstanceOfArtist_Artist()
+    {
+      Artist newArtist = new Artist("test");
+      Assert.AreEqual(typeof(Artist), newArtist.GetType());
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllArtistObjects_ArtistList()
+    {
+      string name01 = "test";
+      string name02 = "test2";
+      Artist newArtist1 = new Artist(name01);
+      Artist newArtist2 = new Artist(name02);
+      List<Artist> newList = new List<Artist> { newArtist1, newArtist2 };
+      List<Artist> result = Artist.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
+    {
+      string title = "test";
+      Artist newArtist = new Artist(title);
+      int result = newArtist.Id;
+      Assert.AreEqual(1, result);
+    }
+  }
+}
--- MusicOrganizer/Models/Artist.cs
+++ MusicOrganizer/Models/Artist.cs
@@ -1,8 +1,28 @@
+using System.Collections.Generic;
+
 namespace MusicOrganizer.Models
 {
   public class Artist
   {
-    public string Recipient { get; set; }
-    public string Sender { get; set; }
+    private static List<Artist> _instances = new List<Artist> {};
+    public string Name { get; set; }
+    public int Id { get; }
+
+    public Artist(string name)
+    {
+      Name = name;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static List<Artist> GetAll()
+    {
+      return _instances;
+    }
   }
 }
