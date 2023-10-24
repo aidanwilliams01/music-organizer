253c2d02e89a9ef83631b87d71fb700f65913997
Aidan Williams <aidanwilliams01@gmail.com>
2023-10-24 15:34:01 -0700

    pass GetAll and GetId album tests

--- MusicOrganizer.Tests/ModelTests/MusicOrganizerTests.cs
+++ MusicOrganizer.Tests/ModelTests/MusicOrganizerTests.cs
@@ -1,17 +1,45 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MusicOrganizer.Models;
+using System.Collections.Generic;
 
 namespace MusicOrganizer.Tests
 {
   [TestClass]
-  public class AlbumTests
+  public class AlbumTests : IDisposable
   {
+
+    public void Dispose()
+    {
+      Album.ClearAll();
+    }
 
     [TestMethod]
     public void AlbumConstructor_CreatesInstanceOfAlbum_Album()
     {
-      Album newAlbum = new Album();
+      Album newAlbum = new Album("test");
       Assert.AreEqual(typeof(Album), newAlbum.GetType());
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllAlbumObjects_AlbumList()
+    {
+      string title01 = "test";
+      string title02 = "test2";
+      Album newAlbum1 = new Album(title01);
+      Album newAlbum2 = new Album(title02);
+      List<Album> newList = new List<Album> { newAlbum1, newAlbum2 };
+      List<Album> result = Album.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void GetId_ItemsInstantiateWithAnIdAndGetterReturns_Int()
+    {
+      string title = "test";
+      Album newAlbum = new Album(title);
+      int result = newAlbum.Id;
+      Assert.AreEqual(1, result);
     }
   }
 }
--- MusicOrganizer/Models/Album.cs
+++ MusicOrganizer/Models/Album.cs
@@ -1,13 +1,28 @@
+using System.Collections.Generic;
+
 namespace MusicOrganizer.Models
 {
   public class Album
   {
-    // public string Recipient { get; set; }
-    // public string Sender { get; set; }
+    private static List<Album> _instances = new List<Album> {};
+    public string Title { get; set; }
+    public int Id { get; }
 
-    public Album()
+    public Album(string title)
     {
-      
+      Title = title;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+
+    public static List<Album> GetAll()
+    {
+      return _instances;
     }
   }
 }
